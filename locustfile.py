b4bcf7a832de376f391ffabae126d2106a77cce9
IgorBrat <akbars5678@gmail.com>
2023-10-21 17:08:46 +0300

    Init project

---
+++ locustfile.py
@@ -0,0 +1,28 @@
+import os
+
+import gevent
+import locust
+
+endpoints = [
+    '/agencies',
+    '/animators',
+    '/cities',
+    '/clientCards',
+    '/clients',
+    '/equipments',
+    '/equipmentShops',
+    '/events',
+    '/orders',
+    '/users',
+]
+
+
+class UserPool(locust.FastHttpUser):
+    @locust.task
+    def flood_requests(self):
+        def auth(endpoint):
+            self.client.get(endpoint, auth=(os.environ["NAME"], os.environ["PASSWORD"]))
+        pool = gevent.pool.Pool()
+        for endpoint in endpoints:
+            pool.spawn(auth, endpoint)
+        pool.join()
