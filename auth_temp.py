c23101b17eaa893aa21f09d4f8b0d0cc38ceec69
IgorBrat <akbars5678@gmail.com>
2023-11-17 10:30:41 +0200

    Sending all types of messages

--- auth_temp.py
+++ auth_temp.py
@@ -20,6 +20,24 @@ def generate_temperature(last_temp, min_threshold=0, max_threshold=60):
     return round(temp, 3)
 
 
+def generate_humidity(last_humidity, min_threshold=0, max_threshold=100):
+    humidity = last_humidity + (random.random() - 0.5) * 0.5
+    if humidity > max_threshold:
+        return max_threshold
+    if humidity < min_threshold:
+        return min_threshold
+    return humidity
+
+
+def generate_heartbeat(last_heartbeat, min_threshold=60, max_threshold=180):
+    heartbeat = last_heartbeat + random.randint(-2, 2)
+    if heartbeat > max_threshold:
+        return max_threshold
+    if heartbeat < min_threshold:
+        return min_threshold
+    return heartbeat
+
+
 # Auth
 credentials_path = r"./resources/creds.json"
 
@@ -32,7 +50,6 @@ if not args.project_id:
     raise ValueError('Specify project id')
 if not args.topic_id:
     raise ValueError('Specify topic id')
-
 
 target_url = f"https://pubsub.googleapis.com/v1/projects/{args.project_id}/topics/{args.topic_id}:publish"
 
@@ -95,9 +112,10 @@ def check():
     return flask.Response(check_body, HTTPStatus.OK)
 
 
-@app.get("/send/temp")
+@app.get("/send/temperature")
 def send_messages():
     num = int(flask.request.args.get('num'))
+    delay_ms = int(flask.request.args.get('delay'))
     global curr_temp
     resp_to_return = {}
     resps = []
@@ -112,8 +130,62 @@ def send_messages():
             }
 
             resps.append(publish_message(data).request.body.decode('utf8'))
-            time.sleep(20 * 1e-3)
+            time.sleep(delay_ms * 1e-3)
             curr_temp = generate_temperature(curr_temp)
+        except Exception as e:
+            print(f'Exception: {e}')
+            return flask.Response(status=HTTPStatus.BAD_REQUEST)
+    resp_to_return['request_body'] = resps
+    return flask.Response(json.dumps(resp_to_return), status=HTTPStatus.OK)
+
+
+@app.get("/send/humidity")
+def send_messages():
+    num = int(flask.request.args.get('num'))
+    delay_ms = int(flask.request.args.get('delay'))
+    global curr_humidity
+    resp_to_return = {}
+    resps = []
+    for _ in range(num):
+        try:
+            data = {
+                "type": "HUM",
+                "value": curr_humidity,
+                "datetime": str(datetime.datetime.now()),
+                "latitude": latitude2 + (random.random() - 0.5) * 1e-2,
+                "longitude": longitude2 + (random.random() - 0.5) * 1e-2,
+            }
+
+            resps.append(publish_message(data).request.body.decode('utf8'))
+            time.sleep(delay_ms * 1e-3)
+            curr_humidity = generate_humidity(curr_humidity)
+        except Exception as e:
+            print(f'Exception: {e}')
+            return flask.Response(status=HTTPStatus.BAD_REQUEST)
+    resp_to_return['request_body'] = resps
+    return flask.Response(json.dumps(resp_to_return), status=HTTPStatus.OK)
+
+
+@app.get("/send/heartbeat")
+def send_messages():
+    num = int(flask.request.args.get('num'))
+    delay_ms = int(flask.request.args.get('delay'))
+    global curr_heartbeat
+    resp_to_return = {}
+    resps = []
+    for _ in range(num):
+        try:
+            data = {
+                "type": "HB",
+                "value": curr_heartbeat,
+                "datetime": str(datetime.datetime.now()),
+                "latitude": latitude3 + (random.random() - 0.5) * 1e-2,
+                "longitude": longitude3 + (random.random() - 0.5) * 1e-2,
+            }
+
+            resps.append(publish_message(data).request.body.decode('utf8'))
+            time.sleep(delay_ms * 1e-3)
+            curr_heartbeat = generate_heartbeat(curr_heartbeat)
         except Exception as e:
             print(f'Exception: {e}')
             return flask.Response(status=HTTPStatus.BAD_REQUEST)
